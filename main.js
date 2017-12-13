76d779a3977c0fc04c54a1007b6207a09b537f9a
Benjie <benjiecodes@gmail.com>
2017-12-13 12:46:10 -0500

    added flex properties and vanilla JS to toggle styles

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hey');
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(function (panel) {
+  panel.addEventListener('click', toggleOpen)
+});
+panels.forEach(function (panel) {
+  panel.addEventListener('transitionend', toggleActive)
+});
